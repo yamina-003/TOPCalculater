248c35431c26e5f50508c9c4ed3548800a59f7b6
yamina-003 <yaminabentaifour@gmail.com>
2025-12-03 20:54:33 +0100

    Make and style the intial calculator layout

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+function add(x, y ){
+   return x + y;
+}
+
+function substract(x, y){
+    return x - y;
+}
+
+function multiply(x, y){
+    return x*y;
+}
+
+function divide(x, y){
+    if (y !== 0){
+        return x / y;
+    }
+
+    return 'ERROR';
+}
+
